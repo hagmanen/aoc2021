2e6f91056c5c0944d5ee2b2be05746ea6dd5f842
Fredrik Hagman <fhagman@spotify.com>
2021-12-10 22:34:30 +0100

    improve day9

--- day9.hs
+++ day9.hs
@@ -2,9 +2,7 @@ import System.IO
     ( IOMode(ReadMode), hClose, hGetContents, openFile )  
 import Data.List.Split (splitOn)
 import Data.List (sort)
-import Data.Sequence (update, fromList)
-import Data.Foldable (toList)
-import Distribution.Simple.LocalBuildInfo (absoluteComponentInstallDirs)
+import Data.List.Index (setAt, modifyAt)
 import Data.Bifunctor (first, second, bimap)
 
 parseHights :: String -> [Int]
@@ -32,44 +30,39 @@ solve m = sum [1 + getHight x y m | x <- [0..width], y <- [0..hight], isLowPoint
   where hight = length m - 1
         width = length (head m) - 1
 
-parseWalls :: String -> [Bool]
-parseWalls = map (\x -> (read [x] :: Int) == 9)
+parseBasins :: String -> [Bool]
+parseBasins = map (\x -> (read [x] :: Int) == 9)
 
 result :: [Int] -> Int
-result l = product (take 3 (reverse (sort l)))
+result l = product (take 3 (reverse (sort [x | x <-l, x/= 0])))
 
-setAbove :: Int -> Int -> [Int] -> [Int]
-setAbove x val above = toList (update x val $ fromList above)
+replace :: Int -> Int -> [Int] -> [Int]
+replace old new l = [if x == old then new else x | x <- l]
 
-replaceAbove :: [Int] -> Int -> [Int] -> [Int]
-replaceAbove old new l = [if x `elem` old then new else x | x <- l]
+incAt :: Int -> [Int] -> [Int]
+incAt x = modifyAt x (+1)
 
-incBasin :: Int -> [Int] -> [Int]
-incBasin x basins = toList (update x val $ fromList basins)
-  where val = (basins !! x) + 1
+clearAt :: Int -> [Int] -> [Int]
+clearAt x = setAt x 0
 
-clearBasins :: [Int] -> [Int] -> [Int]
-clearBasins xs b = foldl (\ b x -> toList (update x 0 $ fromList b)) b xs
-
-mergeBasins :: Int -> Int -> [Int] -> [Int]
-mergeBasins a b basins = clearBasins [a,b] basins ++ [(basins !! a) + (basins !! b) + 1]
+mergeAndInc :: Int -> Int -> [Int] -> [Int]
+mergeAndInc a b list = setAt b ((list !! a) + (list !! b) + 1) (clearAt a list)
 
 fillRow :: [Int] -> Int -> [Bool] -> ([Int],[Int]) -> ([Int],[Int])
-fillRow [] _ _ above_basins = above_basins
-fillRow (x:xs) prev row above_basins
-  | row !! x =                      fillRow xs 0 row (first (setAbove x 0) above_basins)
-  | prev == 0 && above_basin == 0 = fillRow xs next_basin row (setAbove x next_basin (fst above_basins), snd above_basins ++ [1])
-  | prev == 0 && above_basin /= 0 = fillRow xs above_basin row (second (incBasin above_basin) above_basins)
-  | prev /= 0 && above_basin == 0 = fillRow xs prev row (bimap (setAbove x prev) (incBasin prev) above_basins)
-  | prev == above_basin = fillRow xs above_basin row (second (incBasin above_basin) above_basins)
-  | otherwise = fillRow xs next_basin row (bimap (replaceAbove [above_basin,prev] next_basin) (mergeBasins prev above_basin) above_basins)
-  where next_basin = length (snd above_basins)
-        above_basin = fst above_basins !! x
+fillRow [] _ _ above = above
+fillRow (x:xs) left row above
+  | row !! x =                fillRow xs 0 row (first (setAt x 0) above)
+  | left == 0 && up == 0 =    fillRow xs next row (setAt x next (fst above), snd above ++ [1])
+  | left == 0 || left == up = fillRow xs up row (second (incAt up) above)
+  | left /= 0 && up == 0 =    fillRow xs left row (bimap (setAt x left) (incAt left) above)
+  | otherwise =               fillRow xs up row (bimap (replace left up) (mergeAndInc left up) above)
+  where next = length (snd above)
+        up = fst above !! x
 
 fill :: [[Bool]] -> ([Int],[Int]) -> Int
-fill [] above_basins = result (snd above_basins)
-fill (row:rows) above_basins = fill rows above_basins'
-  where above_basins' = fillRow [0..(length row - 1)] 0 row above_basins
+fill [] above = result (snd above)
+fill (row:rows) above = fill rows above'
+  where above' = fillRow [0..(length row - 1)] 0 row above
 
 solve2 :: [[Bool]] -> Int
 solve2 m = fill m (replicate (length (head m)) 0, [0])
@@ -79,7 +72,7 @@ main = do
         handle <- openFile "input9.txt" ReadMode
         contents <- hGetContents handle
         print (solve [parseHights row | row <- lines contents])
-        print (solve2 [parseWalls row | row <- lines contents])
+        print (solve2 [parseBasins row | row <- lines contents])
         hClose handle   
 
 -- 572
