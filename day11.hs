0cf2f2307a6e0c6e5ae380e8f65e5e8b9b978623
Fredrik Hagman <fhagman@spotify.com>
2021-12-11 20:30:10 +0100

    day11 list instead of matrix

--- day11.hs
+++ day11.hs
@@ -8,71 +8,64 @@ import Data.List.Split(chunksOf)
 string2Ints :: String -> [Int]
 string2Ints = map (\x -> read [x] :: Int)
 
-countFlashes :: [[Int]] -> Int
-countFlashes m = sum [sum [if x == 0 then 1 else 0 | x <- row] | row <- m]
+countZeros :: [Int] -> Int
+countZeros l = length $ filter (==0) l
 
-toCoord :: Maybe Int -> Int -> Maybe (Int, Int)
-toCoord Nothing _ = Nothing
-toCoord (Just x) l = Just (mod x l, div x l)
+incAt :: [Int] -> [Int] -> [Int]
+incAt [] l = l
+incAt (x:xs) l
+  | (l!!x) == 0 = incAt xs l
+  | otherwise = incAt xs (modifyAt x (+1) l)
 
-findFlash :: [[Int]] -> Maybe (Int, Int)
-findFlash m = toCoord (findIndex (>9) (concat m)) (length (head m))
+clearAt :: Int -> [Int] -> [Int]
+clearAt x = setAt x 0
 
-incAt :: (Int, Int) -> [[Int]] -> [[Int]]
-incAt (x,y) m
-  | x == -1 = m
-  | y == -1 = m
-  | x == w = m
-  | y == length m = m
-  | (m!!y)!!x == 0 = m
-  | otherwise = chunksOf w (modifyAt (x + y * w) (+1) (concat m))
-    where w = length (head m)
+xIf :: Int -> Bool -> [Int]
+xIf _ False = []
+xIf x True = [x]
 
-clearAt :: (Int, Int) -> [[Int]] -> [[Int]]
-clearAt (x,y) m = chunksOf w (setAt (x + y * w) 0 (concat m))
-  where w = length (head m)
+neighbors :: Int -> Int -> [Int]
+neighbors x w = concat [xIf (x - w) over,  xIf (x - w + 1) (over && right),
+                        xIf (x + 1) right, xIf (x + 1 + w) (right && under),
+                        xIf (x + w) under, xIf (x + w - 1) (under && left),
+                        xIf (x - 1) left,  xIf (x - 1 - w) (left && over)]
+  where over = div x w /= 0
+        under = div x w /= (w-1)
+        left = mod x w /= 0
+        right = mod x w /= (w-1)
 
-doFlash :: Maybe(Int, Int) -> [[Int]] -> [[Int]]
-doFlash Nothing m = m
-doFlash (Just (x, y)) m = m9
-  where m1 = incAt (x-1, y-1) m
-        m2 = incAt (x, y-1) m1
-        m3 = incAt (x+1, y-1) m2
-        m4 = incAt (x-1, y) m3
-        m5 = clearAt (x, y) m4
-        m6 = incAt (x+1, y) m5
-        m7 = incAt (x-1, y+1) m6
-        m8 = incAt (x, y+1) m7
-        m9 = incAt (x+1, y+1) m8
+doFlash :: Maybe Int -> [Int] -> Int -> [Int]
+doFlash Nothing l _ = l
+doFlash (Just x) l w =  incAt n (clearAt x l)
+  where n = neighbors x w
 
-flash :: [[Int]] -> [[Int]]
-flash m
-  | isNothing f = m
-  | otherwise = flash (doFlash f m)
-  where f = findFlash m
+flash :: [Int] -> Int -> [Int]
+flash l w
+  | isNothing f = l
+  | otherwise = flash (doFlash f l w) w
+  where f = findIndex (>9) l
 
-step :: ([[Int]], Int) -> ([[Int]], Int)
-step (m, f) = (m'', f + countFlashes m'')
-  where m' = [[x+1 | x <- row] | row <- m]
-        m'' = flash m'
+step :: ([Int], Int) -> Int -> ([Int], Int)
+step (l, f) w = (l', f + countZeros l')
+  where l' = flash [x+1 | x <- l] w
 
-solve :: ([[Int]], Int) -> Int -> ([[Int]], Int)
-solve state 0 = state
-solve state i = solve (step state) (i-1)
+solve :: ([Int], Int) -> Int -> Int -> ([Int], Int)
+solve state _ 0 = state
+solve state w i = solve (step state w) w (i-1)
 
-solve2 :: [[Int]] -> Int -> Int
-solve2 m s
-  | snd m' == length m * length (head m) = s
-  | otherwise = solve2 (fst m') (s+1)
-  where m' = step (m,0)
+solve2 :: [Int] -> Int -> Int -> Int
+solve2 l w s
+  | snd l' == length l = s
+  | otherwise = solve2 (fst l') w (s+1)
+  where l' = step (l,0) w
 
 main :: IO ()
 main = do  
         handle <- openFile "input11.txt" ReadMode
         contents <- hGetContents handle
         let input = [string2Ints row | row <- lines contents]
-        print (snd (solve (input, 0) 100))
-        print (solve2 input 1)
+        print (snd (solve (concat input, 0) (length (head input)) 100))
+        print (solve2 (concat input) (length (head input)) 1)
         hClose handle   
 
 -- 1627
