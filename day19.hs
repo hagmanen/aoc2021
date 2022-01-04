d0deaf2f265ccdcc0277a75080e4b92b4a609dc5
Fredrik Hagman <fhagman@spotify.com>
2022-01-04 12:28:00 +0100

    day19

--- day19.hs
+++ day19.hs
@@ -1,114 +1,116 @@
 import System.IO
     ( IOMode(ReadMode), hClose, hGetContents, openFile )
+import Data.List (sort, nub)
 import Data.List.Split (splitOn)
-import qualified Data.Set as Set
-import Data.List (permutations)
+import Debug.Trace (trace)
 
-data Coord = Coord { x :: Int,
-                     y :: Int,
-                     z :: Int } deriving (Eq, Ord)
+type Coord = (Int,Int,Int)
 
-data Match = NoMatch | YesMatch Int Int deriving (Show, Eq)
+allBeacons :: [[Coord]] -> ([Coord], [Coord])
+allBeacons scanners = accMatches (sort (head scanners), [(0,0,0)]) (tail scanners)
 
-instance Show Coord where
-    show c = "[" ++ show (x c) ++ "," ++ show (y c) ++ "," ++ show (z c) ++ "]"
+merge :: [Coord] -> [Coord] -> [Coord]
+merge (a:as) (b:bs)
+  | a == b = a:merge as bs
+  | a < b = a:merge as (b:bs)
+  | otherwise = b:merge (a:as) bs
+merge as [] = as
+merge [] bs = bs
 
-createCoord :: [String] -> Coord
-createCoord ss = Coord (read (head ss)) (read (ss !! 1)) (read (ss !! 2))
+accMatches :: ([Coord], [Coord]) -> [[Coord]] -> ([Coord], [Coord])
+accMatches matched scanners | trace ("Unmatched scanners: " ++ show (length scanners) ++ ", Matched scanners: " ++ show (length (snd matched))) False = undefined
+accMatches ([],_) _ = error "Ran out of root nodes"
+accMatches matched_beacons [] = matched_beacons
+accMatches matched_beacons (scanner:scanners)
+  | m = accMatches (matched_beacons', offsets) scanners
+  | otherwise = accMatches matched_beacons (scanners ++ [scanner])
+  where (m, rotated_beacons) = match (fst matched_beacons) (allRotations scanner)
+        matched_beacons' = merge (fst matched_beacons) (snd rotated_beacons)
+        offsets = fst rotated_beacons : snd matched_beacons
 
-toList :: Coord -> [Int]
-toList c = [x c, y c, z c]
+applyOffset :: Coord -> [[Coord]] -> [[Coord]]
+applyOffset offset = map (map (add offset))
 
-fromList :: [Int] -> Coord
-fromList l = Coord (head l) (l !! 1) (l !! 2)
+match :: [Coord] -> [[Coord]] -> (Bool, (Coord, [Coord]))
+match _ [] = (False, ((0,0,0),[]))
+match matched_beacons (rotated_beacons:rbs)
+  | m = (True, new_beacons)
+  | otherwise = match matched_beacons rbs
+  where (m, new_beacons) = matchOffset matched_beacons (allOffsets matched_beacons rotated_beacons)
 
-parse :: [String] -> [Coord] -> [Coord]
-parse ss cs = foldr ((:) . createCoord . splitOn ",") cs ss
+matchOffset :: [Coord] -> [(Coord, [Coord])] -> (Bool, (Coord, [Coord]))
+matchOffset _ [] = (False, ((0,0,0),[]))
+matchOffset matched_beacons (off:offs)
+  | c >= 12 = (True, off)
+  | otherwise = matchOffset matched_beacons offs
+  where c = countMatches matched_beacons (snd off) 0
 
-allFlips :: [[Int]]
-allFlips = mapM (const [-1,1]) [1..3]
+countMatches :: [Coord] -> [Coord] -> Int -> Int
+countMatches (a:as) (b:bs) m
+  | a == b = countMatches as bs (m+1)
+  | a < b = countMatches as (b:bs) m
+  | otherwise = countMatches (a:as) bs m
+countMatches _ _ m = m
 
-allPermutations :: Coord -> [[Int]]
-allPermutations c = permutations (toList c)
+sub :: Coord -> Coord -> Coord
+sub (x1,y1,z1) (x2,y2,z2) = (x1-x2, y1-y2, z1-z2)
 
-multiC :: [Int] -> [Int] -> [Int]
-multiC [x1,y1,z1] [x2,y2,z2] = [x1*x2,y1*y2,z1*z2]
-multiC _ _ = [0,0,0]
+add :: Coord -> Coord -> Coord
+add (x1,y1,z1) (x2,y2,z2) = (x1+x2, y1+y2, z1+z2)
 
-multiS :: [Int] -> [[Int]] -> [[Int]] -> [[Int]]
-multiS _ [] r = r
-multiS c (f:fs) r = multiS c fs (r ++ [multiC c f])
+allOffsets :: [Coord] -> [Coord] -> [(Coord, [Coord])]
+allOffsets matched_beacons scanner = map (\o -> (o, map (add o) scanner)) offsets
+  where offsets = nub [sub m b| m <- matched_beacons, b <- scanner]
 
-multi :: [[Int]] -> [[Int]] -> [[Int]] -> [Coord]
-multi [] _ r = [fromList x | x <- r]
-multi (c:cs) fs r = multi cs fs (r ++ multiS c fs [])
+allRotations :: [Coord] -> [[Coord]]
+allRotations scanner = map (\f -> sort $ map f scanner) cubeRotations
 
-allRotations :: Coord -> [Coord]
-allRotations c = multi (allPermutations c) allFlips []
+cubeRotations :: [Coord -> Coord]
+cubeRotations = concat [t1,t2,t3,t4,t5,t6]
+  where t1 = repeatF 4 xTurn
+        t2 = map (. zTurn) t1
+        t3 = map (. (zTurn . zTurn)) t1
+        t4 = map (. (zTurn . zTurn . zTurn)) t1
+        t5 = map (. yTurn) t1
+        t6 = map (. (yTurn . yTurn . yTurn)) t1
 
-subCoord :: Coord -> Coord -> Coord
-subCoord c1 c2 = Coord (x c1 - x c2) (y c1 - y c2) (z c1 - z c2)
+repeatF :: Int -> (Coord -> Coord) -> [Coord -> Coord]
+repeatF n f = scanl (\acc _ -> f . acc) id [0..(n-2)]
 
-addCoord :: Coord -> Coord -> Coord
-addCoord c1 c2 = Coord (x c1 + x c2) (y c1 + y c2) (z c1 + z c2)
+xTurn :: Coord -> Coord
+xTurn (x,y,z) = (x,z,-y)
 
-transformers ::[Coord] ->  Coord -> [Coord]
-transformers cs c = [subCoord x c | x <- cs]
+yTurn :: Coord -> Coord
+yTurn (x,y,z) = (-z,y,x)
 
-rotation :: [Coord] -> Int -> [Coord]
-rotation cs n = [allRotations c !! n | c <- cs]
+zTurn :: Coord -> Coord
+zTurn (x,y,z) = (y,-x,z)
 
-rot :: [Coord] -> (Coord, Int) -> [[Coord]]
-rot cs (c, n) = [[addCoord t x | x <- cs] | t <- transformers cs c]
+parseCoord :: String -> Coord
+parseCoord s = (head ns, ns !! 1, last ns)
+  where ns = map read (splitOn "," s)
 
-commonMatch :: Coord -> [Coord] -> [Coord] -> Int
-commonMatch c cs1 cs2 = Set.size (Set.intersection (Set.fromList cs1) (Set.fromList cs2))
+parseScanner :: String -> [Coord]
+parseScanner s = map parseCoord (tail (lines s))
 
+parse :: String -> [[Coord]]
+parse s = map parseScanner (splitOn "\n\n" s)
 
---                current ->Orig -> All rotations -> Match
-maxCountCommon :: Coord -> [Coord] -> [[Coord]] -> Match
-maxCountCommon _ _ [] = NoMatch
-maxCountCommon c cs1 (cs2:cs2s)
-  | match == NoMatch = maxCountCommon c cs1 cs2s
-  | otherwise = match
-    where match = commonMatch c cs1 cs2
+dist :: Coord -> Coord -> Int
+dist (x1,y1,z1) (x2,y2,z2) = abs (x1-x2) + abs (y1-y2) + abs (z1-z2)
 
-findMatch2 :: [Int] -> Coord -> [Coord] -> [Coord] -> (Bool, Int, Int)
-findMatch2 [] _ _ _ = (False, 0, 0)
-findMatch2 (n:ns) c cs1 cs2
-  | count = (True, trans, n)
-  | otherwise = findMatch2 ns c cs1 cs2
-    where (count, trans) = maxCountCommon cs1 (rot cs2 (c, n)) 0
-
-findMatch1 :: Coord -> [Coord] -> [Coord] -> (Bool, (Int, Int))
-findMatch1 _ [] _ = (False, (0, 0))
-findMatch1 c cs1 cs2
-  | m = (True, (t, n))
-  | otherwise = (False, (0,0))
-  where (m, t, n) = findMatch2 [0..41] c cs1 cs2
-
-findMatch :: [Coord] -> [Coord] ->[Coord] -> (Int, Int)
-findMatch [] _ _ = (0, -1)
-findMatch (c1:cs1) ocs1 cs2
-  | found = result
-  | otherwise = findMatch cs1 ocs1 cs2
-  where (found, result) = findMatch1 c1 ocs1 cs2 
-
-s0Coords :: [Coord] -> (Coord, Int) -> [Coord]
-s0Coords  cs (t,n) = [addCoord t c | c <- cs']
-    where cs' = [allRotations c !! n | c <- cs]
+dists :: [Coord] -> [Int]
+dists ss = [dist x y | x <- ss, y <- ss]
 
 main :: IO ()
 main = do
         handle <- openFile "input19.txt" ReadMode
         contents <- hGetContents handle
-        let info = splitOn "\n\n" contents
-            probes = [parse (tail (lines s)) [] | s <- info]
---        print probes
-        print (findMatch (head probes) (head probes) (head (tail probes)))
---        print (solve list (initialState list 1))
---        print (solve list (initialState list 3))
+        let scanners = parse contents
+            (beacons,offsets) = allBeacons scanners
+        print (length beacons)
+        print (maximum (dists offsets))
         hClose handle
 
--- 
--- 
+-- 465
+-- 12149
